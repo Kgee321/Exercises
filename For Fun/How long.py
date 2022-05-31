db4ab7cc6f8904c977e90283440208bf0a4ae311
Kgee321 <geek@middleton.school.nz>
2022-06-01 09:49:56 +1200

    Study for comp

---
+++ For Fun/How long.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+def date_time():
+    time = datetime.now()
+    print(time)
+
+
+date_time()
+
+for i in range(10):
+    print(i)
+
+date_time()
+
+
+""" Up to 31st video: https://www.youtube.com/watch?v=sKW-zdYZNX4"""
